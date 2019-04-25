cdb2a06d88e28b9ec914184c96d1d87b2052b9ce
William Diebolt <william@Williams-MacBook-Pro.local>
2019-04-25 07:01:45 -0700

    disconnecting removes players

--- server.js
+++ server.js
@@ -15,42 +15,40 @@ app.use('/static', express.static(__dirname + '/static'));
 
 // Routing
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, 'index.html'));
+	response.sendFile(path.join(__dirname, 'index.html'));
 });
 
 server.listen(PORT, function() {
-  console.log('Starting server on port 5000');
+	console.log('Starting server on port 5000');
 });
 
 var players = {};
 io.on('connection', function(socket) {
-  
-	console.log('an user connected');
-  	socket.on('disconnect', function(){
-    	console.log('user disconnected');
+	socket.on('disconnect', function(){
+		delete players[socket.id];     	
 	})
 
-  socket.on('new player', function() {
-    players[socket.id] = {
-      x: 800 * Math.random(),
-      y: 600 * Math.random()
-    };
-  });
-  socket.on('movement', function(data) {
-    var player = players[socket.id] || {};
-    if (data.left) {
-      player.x -= 5;
-    }
-    if (data.up) {
-      player.y -= 5;
-    }
-    if (data.right) {
-      player.x += 5;
-    }
-    if (data.down) {
-      player.y += 5;
-    }
-  });
+	socket.on('new player', function() {
+		players[socket.id] = {
+			x: 800 * Math.random(),
+			y: 600 * Math.random()
+		};
+	});
+	socket.on('movement', function(data) {
+		var player = players[socket.id] || {};
+		if (data.left) {
+			player.x -= 5;
+		}
+		if (data.up) {
+			player.y -= 5;
+		}
+		if (data.right) {
+			player.x += 5;
+		}
+		if (data.down) {
+			player.y += 5;
+		}
+	});
 });
 
 setInterval(function() {
